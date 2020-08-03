aa85ed6576e297f8937c75d4212bc0c3668f6dfb
Lukas Bonrath <ludwig.germersheim@gmail.com>
2020-08-03 21:48:15 +0200

    added webview

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ if ($conn->connect_error) {
 $sql  = 'SELECT * FROM `stechzeiten`';
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-    echo "<style>table, th, td {border: 1px solid black;}</style> <br><table><tr><th>Tag</th><th>Uhrzeit</th><th>Status</th></tr>";
+    echo '<head><link rel="stylesheet" href="styles.css"></head><body><br><table class="container"><tr><th><h1>Tag</h1></th><th><h1>Uhrzeit</h1></th><th><h1>Status</h1></th></tr>';
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>". $row["Datum"]. "</td><td>". $row["Uhrzeit"]. "</td><td>" . $row["Status"] . "</td></tr>";
     }
-    echo "</table><br>";
+    echo "</table><br></body>";
 } else {
     echo "0 results";
 }
