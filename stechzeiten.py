34508388392b0e3dd03bb468a4521d7b9cede309
Lukas Bonrath <ludwig.germersheim@gmail.com>
2020-08-03 10:23:20 +0200

    updated readme and added support for mysql database

--- stechzeiten.py
+++ stechzeiten.py
@@ -1,8 +1,18 @@
 from time import sleep  #used for sleep function
+import mysql.connector  #used to connect to mysql database
 import sys              #used for system functions
 import re               #used for regex 
 from mfrc522 import SimpleMFRC522   #used for communication to rfid reader
 reader = SimpleMFRC522()
+
+mydb = mysql.connector.connect( #connect to mysql
+  host="localhost",  #Server who are the databse stored
+  user="stechzeit_nutzer", #user who has rights on the database
+  password="blabla",  #password for user 
+  database="stechzeiten"  #database
+)
+
+cursor = mydb.cursor()
 
 try:
     while True:
@@ -13,14 +23,21 @@ try:
             status_abw = "abwesend" #set new state
             reader.write(status_abw) #write new state
             print("Written")
-            sleep(3) #sleep 3 seconds
+            sql = """INSERT INTO stechzeiten (Datum, Uhrzeit, Status) VALUES (CURRENT_DATE(), CURRENT_TIME(), "abwesend")""" #sql command
         else:
             print("ID: %s\nText: %s" % (id,readed_text))
             status_anw = "anwesend" #set new state
             reader.write(status_anw) #write new state
             print("Written")
-            sleep(3) #sleep 3 seconds
+            sql = """INSERT INTO stechzeiten (Datum, Uhrzeit, Status) VALUES (CURRENT_DATE(), CURRENT_TIME(), "anwesend")""" #sql command
+
+        cursor.execute(sql) #execute sql command
+        mydb.commit()
+        sleep(3) #sleep 3 seconds
+        
 
 except KeyboardInterrupt: #cleanup and unuse GPIOs
     GPIO.cleanup()
+    mydb.close() #close the connection to mysql
     raise
+    
