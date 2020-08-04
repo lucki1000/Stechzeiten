0c6b8beb24c6a9adf28353e062a3f1f0d6a4632f
Lukas Bonrath <ludwig.germersheim@gmail.com>
2020-08-04 10:55:30 +0200

    add sort function to Stechzeit Viewer

--- index.php
+++ index.php
@@ -14,12 +14,12 @@ if ($conn->connect_error) {
 $sql  = 'SELECT * FROM `stechzeiten`';
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-    echo '<head><link rel="stylesheet" href="styles.css"></head><body><br><table class="container"><tr><th><h1>Tag</h1></th><th><h1>Uhrzeit</h1></th><th><h1>Status</h1></th></tr>';
+    echo '<html><head><script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script><title>Simple Stechzeiten Viewer</title><link rel="stylesheet" href="styles.css"></head><body><h1><p class="try">Stechzeiten Viewer</p></h1><br><table class="container searchable sortable"><thead><tr><th><h1>Tag</h1></th><th><h1>Uhrzeit</h1></th><th><h1>Status</h1></th></tr></thead><tbody>';
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>". $row["Datum"]. "</td><td>". $row["Uhrzeit"]. "</td><td>" . $row["Status"] . "</td></tr>";
     }
-    echo "</table><br></body>";
+    echo "<tbody></table><br></body></html>";
 } else {
     echo "0 results";
 }
