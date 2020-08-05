fec7db56fffe82a772eef2e98a8271d738db2030
Lukas Bonrath <ludwig.germersheim@gmail.com>
2020-08-05 12:55:01 +0200

    added test web viewer

--- test/select_query.php
+++ test/select_query.php
@@ -13,7 +13,6 @@ if ($conn->connect_error) {
 
 $sql  = 'SELECT * FROM `stechzeiten`';
 $result = $conn->query($sql);
-echo '<html><head><script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script><title>Simple Stechzeiten Viewer</title><link rel="stylesheet" href="styles.css"></head><body><h1><p class="try">Stechzeiten Viewer</p></h1><br><table class="container searchable sortable"><thead><tr><th><h1>Tag</h1></th><th><h1>Uhrzeit</h1></th><th><h1>Status</h1></th></tr></thead><tbody>';
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -22,7 +21,5 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
-echo "<tbody></table><br></body></html>";
-
 $conn->close();
 ?>
