004e72eda85f586aa224e25a2aeccb9b52197f2c
Lukas Bonrath <ludwig.germersheim@gmail.com>
2020-08-14 16:44:24 +0200

    fixed install.sh and create_database.sh

--- create_database.sh
+++ create_database.sh
@@ -1,6 +1,8 @@
-sudo mysql -uroot -e "CREATE USER 'stechzeit_nutzer'@'localhost' IDENTIFIED BY 'blabla'"
-sudo mysql -uroot -e "CREATE DATABASE stechzeiten"
-sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON stechzeiten.* TO 'stechzeit_nutzer'@'localhost'"
-sudo mysql -uroot -e "CREATE TABLE stechzeiten ( ID bigint, Datum date, Uhrzeit time, Status text )"
-sudo mysql -uroot -e "FLUSH PRIVILEGES"
-
+sudo mysql -uroot <<MYSQL_SCRIPT
+CREATE USER 'stechzeit_nutzer'@'localhost' IDENTIFIED BY 'blabla';
+CREATE DATABASE stechzeiten;
+GRANT ALL PRIVILEGES ON stechzeiten.* TO 'stechzeit_nutzer'@'localhost';
+USE stechzeiten;
+CREATE TABLE stechzeiten ( ID bigint, Datum date, Uhrzeit time, Status text );
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
--- install.sh
+++ install.sh
@@ -20,16 +20,21 @@ sudo apt install mariadb-common mariadb-server python3-dev python3-pip tmux -y
 sudo pip3 install spidev
 sudo pip3 install mfrc522
 
+#create database and user
+chmod +x create_database.sh
+./create_database.sh
+
 #define WebRoot and copy Webviewer to webroot
 read -p "Type your webroot, common webroot from Apache2 is /var/www/html/: " webroot_path
-sudo cp -r website/* $(webroot_path)
+sudo cp -r website/* $webroot_path
 
 #add a new service file to systemd and enable on startup
-sudo cp -r systemd_unit/stechzeiten.service /usr/lib/systemd/user/
+sudo cp -r systemd_unit/stechzeiten.service /etc/systemd/system/
 sudo systemctl enable stechzeiten.service
 
 #copy stechzeiten(folder) to /etc/ here are the needed files to run the python script
-sudo mv stechzeiten /etc/
+sudo cp -r stechzeiten /etc/
 
 #cleanup not longer needed files
+sudo rm -- "$0"
 sudo rm -rf ../Stechzeiten
