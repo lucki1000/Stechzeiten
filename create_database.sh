069076b261aeb439448e7781ecf18898d4f0222c
Lukas Bonrath <ludwig.germersheim@gmail.com>
2020-08-16 13:54:21 +0200

    updated

--- create_database.sh
+++ create_database.sh
@@ -3,6 +3,6 @@ CREATE USER 'stechzeit_nutzer'@'localhost' IDENTIFIED BY 'blabla';
 CREATE DATABASE stechzeiten;
 GRANT ALL PRIVILEGES ON stechzeiten.* TO 'stechzeit_nutzer'@'localhost';
 USE stechzeiten;
-CREATE TABLE stechzeiten ( ID bigint, Datum date, Uhrzeit time, Status text );
+CREATE TABLE stechzeiten ( ID text, Datum date, Uhrzeit time, Status text );
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
